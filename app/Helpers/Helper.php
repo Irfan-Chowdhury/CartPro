ef640a9f8490061acdbeffe739dffc06a578fd28
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2025-04-15 20:30:04 +0600

    Updated to old version

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -1,6 +1,7 @@
 <?php
 
-if (!function_exists('public_asset')) {
+if (!function_exists('public_asset'))
+{
     function public_asset($path)
     {
         return asset('public/' . ltrim($path, '/'));
